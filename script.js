735ef5678886b61d5b43638ec0bdefd1235b00c9
Ajay Rawat <104298002+AjayRawat0706@users.noreply.github.com>
2025-04-05 22:57:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function toCase(text) {
-  // write your code here
+	if(text.length==0){
+		return "-"
+	}
+   let a=text.toLowerCase(); 
+	let b=text.toUpperCase();
+	return `${a}-${b}`
 }
 
 // DO not change the code below
